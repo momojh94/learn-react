cc7517fa715820cf0448c9c31b252009efb9e8c7
momo <mjh79017901@gmail.com>
2020-12-17 23:57:30 +0900

    Add modern react 1.1~1.8
    모던 리액트 1.1 ~ 1.8 input 상태 관리하기

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import Hello from './Hello';
+import Wrapper from './Wrapper';
+import Counter from './Counter';
+import InputSample from './InputSample';
 import './App.css';
 
 function App() {
+  const name = 'react';
+  const style1 = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24, // 기본 단위 px
+    padding: '1rem' // 다른 단위 사용 시 문자열로 설정
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Wrapper>
+      <Hello name="name1" color="blue" isSpecial={true}/>
+      <Hello color="pink"/>
+      <div>{name}</div>
+      <div className="gray-box"></div>
+      </Wrapper>
+      <Counter />
+
+      <InputSample />
+    </>
   );
 }
 
